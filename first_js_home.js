c250c9e428efcdd2213db8ae6cdbdc56358b0975
Jason Kulak <JasonKulak@yahoo.com>
2023-11-03 18:30:26 -0500

    starting the homework

---
+++ first_js_home.js
@@ -0,0 +1,18 @@
+// Section 1. //
+// 1. DRY stands for Don't Repeat Yourself //
+    // - We should pay attention to this because it's easier to maintain, performs better, more readable, and is less buggy //
+    // - programming tools we've learned to use DRY code are functions //
+
+// 2.   // Const (constant) means that the variable is not going to change (ex. someones name). //
+        // When you use the variable let, you can change that variable throughout the course of the program (ex. a persons age) . //
+        // var (variable) isn't commonly used anymore, but was the oldest way of declaring a variable. //
+
+// Section 2. //
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kelvin';
+const f = false;
+console.log(a<b);
